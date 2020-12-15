a0f95f1c03a0fc8910fce4350ec4bd94eb62f101
Sonal Agrawal <sonalsinghal755@gmail.com>
2020-12-16 01:15:50 +0530

    Refactor to controllers and Starability CSS

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5fd90405d51f7834b5f77eb7',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/random/800x600',
