e1d9156f3993961d2776bcffec28d1f82aa849b3
Sonal Agrawal <sonalsinghal755@gmail.com>
2020-12-17 18:17:08 +0530

    More styling and basic security

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-
 const User = require('../models/user');
 
 module.exports.renderRegister = (req, res) => {
@@ -34,6 +33,7 @@ module.exports.login = (req, res) => {
 
 module.exports.logout = (req, res) => {
     req.logout();
+    // req.session.destroy();
     req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
 }
